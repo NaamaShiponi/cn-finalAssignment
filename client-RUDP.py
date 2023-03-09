7b434999f6d9a5f2011513fc361d8107bb23757c
Naama Shiponi <naamas1234567@gmail.com>
2023-03-09 02:12:19 +0200

    add client and server RUDP wish loss packet

--- client-RUDP.py
+++ client-RUDP.py
@@ -1,45 +1,139 @@
+# sudo tc qdisc add dev lo root netem loss 20%
+#sudo tc qdisc del dev lo root netem
 
 import socket
-address_server = ('localhost', 5000)
+from scapy.all import *
+from time import sleep
+
+# Constants
+IP_ADDRESS='localhost'
+PORT=5000
+address_server = (IP_ADDRESS,PORT)
+
+def createPackage(sock,packet_num,msg):
+    packet_num=packet_num.to_bytes(1, 'little', signed=False)
+    sock.sendto(msg+packet_num,address_server)
+
+
+def getMsgAndPM(data):
+    packet_num = data[-1]
+    msg = data[:-1]
+    return packet_num,msg
+
+
+
+def receiveData(sock, packet_num):
+    sock.settimeout(2)
+    counter = 0
+    
+    while True:
+        try:
+            received_data = sock.recvfrom(2048)
+            packet_num_recieved,data= getMsgAndPM(received_data[0])
+            
+            if packet_num_recieved < packet_num:
+                continue
+            
+            if packet_num != packet_num_recieved:
+                return None, None
+            
+        except socket.timeout as e:
+            err = e.args[0]
+            # this next if/else is a bit redundant, but illustrates how the
+            # timeout exception is setup
+            if err == 'timed out':
+                sleep(1)
+                counter += 1
+                
+                if counter == 2:
+                    print("Didn't receive ACK - resend")
+                    return None, None
+                continue
+            else:
+                print(e)
+                sys.exit(1)
+        except socket.error as e:
+            # Something else happened, handle error, exit, etc.
+            print(e)
+            sys.exit(1)       
+        return received_data  
+
+
+def checkConnection(data, sock, packet_num, msg, last_ack):
+    
+    # Send last ACK
+    if last_ack:
+        createPackage(sock,last_ack[0],last_ack[1])
+        sleep(0.5)
+    
+    for c in range(0,2):
+             
+        if not data:
+            createPackage(sock,packet_num+1,msg)
+            data, address_unchecked = receiveData(sock, packet_num+2)
+        else:
+            return data, address_unchecked 
+    if data:
+        return data, address_unchecked         
+    print('Client left the conversation closing the connection')
+    sock.close()
+    exit()
+
+
 def handshake(sock):
     # send SYN message to server
-    message = "(SYN) message from client"
-    sock.sendto(message.encode(), address_server)
-    print(f"message client: {message}")
+    message = b"(SYN) message from client"
+    createPackage(sock,1,message)
+    print(f"message client:{message.decode()}, package number: 1")
     
-    # receive SYN message from server
-    data, address = sock.recvfrom(1024)
-    print(f"message server: {data.decode()}")
+    # receive SYN, ACK message from server
+    data_unchecked, address_unchecked = receiveData(sock,2)
 
-    # send AKE message to server
-    message = "(AKE) Request from client"
-    sock.sendto(message.encode(), address_server)
-    print(f"message client: {message}")
+    if not data_unchecked:
+        data_unchecked, address_unchecked = checkConnection(data_unchecked, sock, 0, message, None)
+    data = data_unchecked
+    address = address_unchecked
+    sock.settimeout(None)
+    packet_num,data= getMsgAndPM(data)
+    
+    print(f"message server:{data.decode()}, package number: {packet_num}")
+
+    # send ACK message to server
+    message = b"(ACK) Request from client"
+    createPackage(sock,packet_num+1,message)
+    print(f"message client:{message.decode()}, package number: {packet_num+1}")
 
 
 def getSizeFile(sock):
-    # send PSH message for size file to server
-    message = "(PSH) message for the size of the file on the server"
-    sock.sendto(message.encode(), address_server)
-    print(f"message client: {message}")
+    # send DATA message for size file to server
+    message = b"(DATA) message for the size of the file on the server"
+    createPackage(sock,4,message)
+    last_ack = (4,message)
+    print(f"message client:{message.decode()}, package number: 4")
+
+    # receive DATA message size file from server
+    # get DATA,ACK message from client
+    data_unchecked, address_unchecked = receiveData(sock,5)
+
+    if not data_unchecked:
+        data_unchecked, address_unchecked = checkConnection(data_unchecked, sock, 4, message, last_ack)
+    data = data_unchecked
+    address = address_unchecked
+    sock.settimeout(None)
+    
+    packet_num,data= getMsgAndPM(data)
+    # data=int.from_bytes(data, 'little', signed=False)
+    print(f"message server:(DATA,ACK) Request size file: {data.decode()}, package number: {packet_num}")
+    
+    # send ACK message to server
+    message = b"(ACK) Request from client"
+    createPackage(sock,packet_num+1,message)
+    print(f"message client:{message.decode()}, package number: {packet_num+1}")
 
     
-    # receive PSH message size file from server
-    data, address = sock.recvfrom(2048)
-    data=int.from_bytes(data, 'little', signed=False)
-    print(f"message server: (PSH) Request size file: {data}")
-    
-    # send AKE message to server
-    message = "(AKE) Request from client"
-    sock.sendto(message.encode(), address_server)
-    print(f"message client: {message}")
-
-    
-    
-
 def main():
     # create a UDP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)    
     handshake(sock)
     getSizeFile(sock)
     
--- server-RUDP.py
+++ server-RUDP.py
@@ -1,44 +1,135 @@
 import socket
 from scapy.all import *
+from time import sleep
 
+IP_ADDRESS='localhost'
+PORT=5000
+
+
+def createPackage(sock,packet_num,msg,address):
+    packet_num=packet_num.to_bytes(1, 'little', signed=False)
+    address_server = (address[0],address[1])
+    sock.sendto(msg+packet_num,address_server)
+
+
+def getMsgAndPM(data):
+    packet_num = data[-1]
+    msg = data[:-1]
+    
+    return packet_num,msg
+
+
+def receiveData(sock, packet_num):
+    sock.settimeout(2)
+    counter = 0
+    
+    while True:
+        try:
+            received_data = sock.recvfrom(2048)
+            packet_num_recieved,data= getMsgAndPM(received_data[0])
+            
+            if packet_num_recieved < packet_num:
+                continue
+            
+            if packet_num != packet_num_recieved:
+                return None, None
+            
+        except socket.timeout as e:
+            err = e.args[0]
+            # this next if/else is a bit redundant, but illustrates how the
+            # timeout exception is setup
+            if err == 'timed out':
+                sleep(1)
+                counter += 1
+                
+                if counter == 2:
+                    print("Didn't receive ACK - resend")
+                    return None, None
+                continue
+            else:
+                print(e)
+                sys.exit(1)
+        except socket.error as e:
+            # Something else happened, handle error, exit, etc.
+            print(e)
+            sys.exit(1)       
+        return received_data  
+
+
+def checkConnection(data, sock, packet_num, msg, address, last_ack):
+    
+    # Send last ACK
+    createPackage(sock,last_ack[0],last_ack[1],address)
+    sleep(0.5)
+    
+    for c in range(0,2):
+             
+        if not data:
+            createPackage(sock,packet_num+1,msg,address)
+            data, address_unchecked = receiveData(sock, packet_num+2)
+        else:
+            return data, address_unchecked 
+    if data:
+        return data, address_unchecked         
+    print('Client left the conversation closing the connection')
+    sock.close()
+    exit()
+    
+    
 def handshake(sock):
-    # SYN message from client
+    # SYN message from client //1
     data, address = sock.recvfrom(1024)
-    print(f'message client:{data}')
+    packet_num,data= getMsgAndPM(data)
+    print(f'message client:{data.decode()}, package number: {packet_num}')
 
-    # Send a response SYN to the client
-    response = "(SYN) Request from server"
-    sock.sendto(response.encode(), address)
-    print(f'message server:  {response}')
+    # Send a response SYN to the client //2
+    message = b"(SYN,ACK) Request from server"
+    last_ack = (packet_num+1,message)
+    createPackage(sock,packet_num+1,message,address)
+    print(f'message server:{message.decode()}, package number: {packet_num+1}')
 
-    # get AKE message from client
-    data, address = sock.recvfrom(1024)
-    print(f'message client:{data}')
+    # get ACK message from client
+    data_unchecked, address_unchecked = receiveData(sock,packet_num+2)
 
-def SendSizeFile(sock,file_size):
+    if not data_unchecked:
+        data_unchecked, address_unchecked = checkConnection(data_unchecked, sock, packet_num, message, address, last_ack)
+    data = data_unchecked
+    address = address_unchecked
+    sock.settimeout(None)
+    
+    packet_num,data= getMsgAndPM(data)
+    print(f'message client:{data.decode()}, package number: {packet_num}')
+
+
+def SendSizeFile(sock,file_size): 
     # Message for get size of the file from client
     data, address = sock.recvfrom(1024)
-    print(f'message client:{data} ')
-
-    data=file_size.to_bytes(2, 'little', signed=False)
-
+    packet_num,data= getMsgAndPM(data)
+    print(f'message client:{data.decode()}, package number: {packet_num}')
+    
+    message = str(file_size).encode()
+    createPackage(sock,packet_num+1,message, address)
+    last_ack = (packet_num+1,message)
+    
     # Send a response SYN to the client
-    sock.sendto(data, address)
-    print(f'message server:  (PSH) Request size file:{file_size}')
+    print(f'message server:(DATA,ACK) Request size file: {file_size}, package number: {packet_num+1}')
+    
+    # get ACK message from client
+    data_unchecked, address_unchecked = receiveData(sock, packet_num+2)
 
+    if not data_unchecked:
+        data_unchecked, address_unchecked = checkConnection(data_unchecked, sock, packet_num, message, address,last_ack)
+    data = data_unchecked
+    address = address_unchecked
+    sock.settimeout(None)
     
-    # get AKE message from client
-    data, address = sock.recvfrom(1024)
-    print(f'message client:{data}')
-    
-    
+    packet_num,data= getMsgAndPM(data)
+    print(f'message client:{data.decode()}, package number: {packet_num}')
+        
     
 def main():
-    app_server_address = 'localhost'
-    app_server_port = 5000
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((app_server_address, app_server_port))
-
+    sock.bind((IP_ADDRESS, PORT))
     handshake(sock)
     
     with open('example.txt', 'r') as f:
@@ -50,6 +141,5 @@ def main():
 
 
 
-    f.close()
 if __name__ == "__main__":
     main()
